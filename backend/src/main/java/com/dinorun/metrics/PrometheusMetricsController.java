d292e5e499d7050f07245b2f965bbf2926bbe95d
James <james@A3014.local>
2026-03-26 14:39:02 +0900

    Feat: Added Grafana

--- backend/src/main/java/com/dinorun/metrics/PrometheusMetricsController.java
+++ backend/src/main/java/com/dinorun/metrics/PrometheusMetricsController.java
@@ -1,7 +1,6 @@
 package com.dinorun.metrics;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
